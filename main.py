1e02aa24617cafef25f2ee5f3ce85273eb0d3e07
Humberto Gonçalves <humberto.c.goncalves@gmail.com>
2023-12-04 23:35:54 +0000

    Lógica básica implementada

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ class SNAKE:
     def __init__(self):
         self.body = [Vector2(5, 10), Vector2(4, 10), Vector2(3, 10)]
         self.direction = Vector2(1, 0)
+        self.new_block = False
         
     def draw_snake(self):
         for block in self.body:
@@ -20,29 +21,60 @@ class SNAKE:
             pygame.draw.rect(screen, (183, 111, 122), snake_rect)
     
     def move_snake(self):
-        body_copy = self.body[:-1]
+        body_copy = self.body[:] if self.new_block == True else self.body[:-1]
         body_copy.insert(0, body_copy[0] + self.direction)
         self.body = body_copy[:]
+        self.new_block = False
+        
+    def add_block(self):
+        self.new_block = True
 
 class FRUIT:
     def __init__(self):
         # Create an x and y position
-        self.x = random.randint(0, CELL_NUMBER_WIDTH-1)
-        self.y = random.randint(0, CELL_NUMBER_HEIGHT-1)
-        self.pos = Vector2(self.x, self.y)
+        #self.x = random.randint(0, CELL_NUMBER_WIDTH-1)
+        #self.y = random.randint(0, CELL_NUMBER_HEIGHT-1)
+        #self.pos = Vector2(self.x, self.y)
+        self.randomize()
         
     # Draw a square
     def draw_fruit(self):
         fruit_rect = pygame.Rect(int(self.pos.x * CELL_SIZE), int(self.pos.y * CELL_SIZE), CELL_SIZE, CELL_SIZE)
         pygame.draw.rect(screen, (126, 166, 114), fruit_rect)
+    
+    def randomize(self):
+        # Create an arbitrary x and y position
+        self.x = random.randint(0, CELL_NUMBER_WIDTH-1)
+        self.y = random.randint(0, CELL_NUMBER_HEIGHT-1)
+        self.pos = Vector2(self.x, self.y)
+
+class MAIN:
+    def __init__(self):
+        self.snake = SNAKE()
+        self.fruit = FRUIT()
+    
+    def update(self):
+        self.snake.move_snake()
+        self.check_collision()
+        
+    def draw_elements(self):
+        self.fruit.draw_fruit()
+        self.snake.draw_snake()
+        
+    def check_collision(self):
+        if self.fruit.pos == self.snake.body[0]:
+            # Reposition the fruit
+            self.fruit.randomize()
+            # Add another block to the snake
+            self.snake.add_block()
 
 pygame.init()
 screen = pygame.display.set_mode((CELL_SIZE * CELL_NUMBER_WIDTH, CELL_SIZE * CELL_NUMBER_HEIGHT))
 # With this, we fix that the game speed is the same for every computer (does not depend on hardware)
 clock = pygame.time.Clock()
 
-fruit = FRUIT()
-snake = SNAKE()
+#fruit = FRUIT()
+#snake = SNAKE()
 
 #test_surface = pygame.Surface((100,200))
 #test_surface.fill((0,0,255))
@@ -52,6 +84,8 @@ snake = SNAKE()
 
 SCREEN_UPDATE = pygame.USEREVENT        # Timer for our snake (without user interaction)
 pygame.time.set_timer(SCREEN_UPDATE, 150)
+
+main_game = MAIN()
 
 while True:
     
@@ -63,21 +97,22 @@ while True:
             pygame.quit()
             sys.exit()
         if event.type == SCREEN_UPDATE:
-            snake.move_snake()
+            #snake.move_snake()
+            main_game.update()
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_UP:
-                snake.direction = Vector2(0, -1)
+                main_game.snake.direction = Vector2(0, -1)
             if event.key == pygame.K_DOWN:
-                snake.direction = Vector2(0, 1)
+                main_game.snake.direction = Vector2(0, 1)
             if event.key == pygame.K_RIGHT:
-                snake.direction = Vector2(1, 0)
+                main_game.snake.direction = Vector2(1, 0)
             if event.key == pygame.K_LEFT:
-                snake.direction = Vector2(-1, 0)
+                main_game.snake.direction = Vector2(-1, 0)
 
     # Draw all our elements
     screen.fill((175, 215, 70))
-    fruit.draw_fruit()
-    snake.draw_snake()
+    #fruit.draw_fruit()
+    #snake.draw_snake()
     
     #test_rect.right += 1
     #pygame.draw.rect(screen, pygame.Color('red'), test_rect)
@@ -85,5 +120,6 @@ while True:
     #screen.blit(test_surface, (x_pos, 250))
     #screen.blit(test_surface, test_rect)   # blit -> block image transfer      //      position is in the top left corner of the surface
     
+    main_game.draw_elements()
     pygame.display.update()
     clock.tick(FRAMERATE)
